fc29b050bfbc90b7a54460ac265736e0556c713c
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2023-08-12 23:18:26 +0530

    adding channel fetch and stared adding the channel detail component

--- src/container/ChannelDetail.jsx
+++ src/container/ChannelDetail.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+import { Videos, ChannelCard } from '../components'
+import { fetchFromApi } from '../utils/fetchFromApi'
 
 const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+  const { id } = useParams();
+  console.log(channelDetail, 'channelDetail')
+  console.log(videos, 'videos')
+
+  useEffect(() => {
+    fetchFromApi(`channels?part=snippet"&id=${id}`)
+    .then((data) => setChannelDetail(data?.items[0]))
+
+    fetchFromApi(`search?channelId=${id}&part=snippet&order=date`)
+    .then((data) => setChannelDetail(data?.items[0]))
+    .then((data) => setVideos(data?.items))
+  }, [id])
   return (
-    <div>ChannelDetail</div>
+    <Box minHeight='95vh'>
+        <Box>
+          <div style ={{
+            backgroundColor: '#f8ceec',
+            backgroundImage: 'linear-gradient(315deg, #f8ceec 0%, #a88beb 74%)',
+            zIndex: 10,
+            height: '300px'
+
+          }} />
+        </Box>
+    </Box>
   )
 }
 
